8ea1656126b921f6aa1fdb5128b31c8e947617ca
plee99 <peter_lee_16@sfu.ca>
2024-03-27 19:04:19 -0700

    added relevant comments and documentation to functions

--- src/feature_extraction/channel_selection.py
+++ src/feature_extraction/channel_selection.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import mne
 import itertools
 
 def calculate_top_channels(sample_data, n=3):
@@ -18,11 +17,11 @@ def calculate_top_channels(sample_data, n=3):
     list
         List of top channels with highest variance for the single sample
     """
-    sample_array = sample_data.to_numpy() # Convert DataFrame to NumPy array
+    sample_array = sample_data.to_numpy() # DataFrame to NumPy array
     variances = np.var(sample_array, axis=0) 
     top_n_indices = np.argsort(variances)[::-1][:n] # Get indices of top n channels based on variance
     top_n_channels = sample_data.columns[top_n_indices].tolist() 
-
+    
     return top_n_channels
 
 
@@ -44,9 +43,10 @@ def calculate_all_samples(df, sample_ids, n):
     pd.DataFrame
         DataFrame where each row represents a sample and columns represent top channels.
     """
-    top_channels_df = pd.DataFrame(index=sample_ids, columns=range(3))  # Initialize DataFrame with 3 columns
+    top_channels_df = pd.DataFrame(index=sample_ids, columns=range(3))  
     for sample_id in itertools.islice(sample_ids, n):
         eeg_sig = df.loc[[str(sample_id)]]
-        top_channels = calculate_top_channels(eeg_sig)  # Calculate top channels for the sample
-        top_channels_df.loc[sample_id, :3] = top_channels[:3]  # Store top 3 channels in DataFrame
+        top_channels = calculate_top_channels(eeg_sig)  
+        top_channels_df.loc[sample_id, :3] = top_channels[:3]  
+
     return top_channels_df
